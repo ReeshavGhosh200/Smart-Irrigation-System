db4c88a60a2c88e2622606aef1d403476a31b3d8
Reeshav_Ghosh200 <132378233+ReeshavGhosh200@users.noreply.github.com>
2025-01-24 20:58:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const dark = document.getElementById('dark');
 const light = document.getElementById('light');
+const page = document.querySelector('.page');
+const loader = document.querySelector('.loader');
 
 dark.addEventListener('click', () => {
-    document.body.style.backgroundColor = '#121212';
+    document.body.style.backgroundColor = '#1D2A35';
     document.body.style.color = '#f0f0f0';
     dark.style.display = 'none';
     light.style.color = 'black';
@@ -16,3 +18,8 @@ light.addEventListener('click', () => {
     dark.style.color = 'white';
     dark.style.display = 'block';
 });
+
+setTimeout(() => {
+    loader.style.display = 'none';
+    page.style.display = 'block';
+}, 3000)
